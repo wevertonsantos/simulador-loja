d7f7be8a1128658f1ab8b8a8ccef5ab2dbb5a3a6
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-07-11 17:00:52 -0300

    começando a construir funções para funcionamento do programa.

--- loja.py
+++ loja.py
@@ -1,7 +1,3 @@
-class Pessoa:
-    def __init__(self,nome):
-        self.nome = nome
-
 class Produtos:
     def __init__(self,nomes,precos):
         self.nomes = nomes
@@ -34,7 +30,6 @@ class Carrinho:
     def finalizar_compra(self):
         print("Compra finalizada")
 
-pessoa1 = Pessoa("Rodrigo")
 produtos = Produtos(["Lápis","Escova","Microfone"],[2.50,10,45])
 print(produtos.produtos_disponiveis())
 carrinho = Carrinho()
@@ -42,3 +37,16 @@ carrinho.adicionar_produto("Lápis",2.50)
 carrinho.adicionar_produto("Escova",10)
 print(f"Total no carrinho: {carrinho.mostra_total()}")
 print(carrinho.finalizar_compra())
+
+def main():
+    while True:
+        print("1. Ver produtos\n2. Adicionar produto\n3. Ver total do carrinho\n4. Finalizar compra\n5. Sair")
+        try:
+            escolha = int(input("Digite uma opção: "))
+            if escolha == 1:
+                ...
+        except ValueError:
+            print("Você digitou algo errado")
+
+
+main()
