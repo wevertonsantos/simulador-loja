13900af9603d3671292e58e821d4b6fc225040b6
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-07-11 17:17:37 -0300

    reestruturando código, passando dicionário ao invés de lista para produtos.

--- loja.py
+++ loja.py
@@ -1,27 +1,22 @@
 class Produtos:
-    def __init__(self,nomes,precos):
-        self.nomes = nomes
-        self.precos = precos
-    
-    def produtos_disponiveis(self):
-        for i in range(len(self.nomes)):
-            print(f"Produto: {self.nomes[i]}, Preço: {self.precos[i]}")
+    def __init__(self,produtos):
+        self.produtos = produtos
 
-class Carrinho:
-    lista_produtos = []
-    lista_precos = []
+    def produtos_disponiveis(self):
+        for numero_produto in produtos:
+            print(produtos[numero_produto]['nome'])
+            print(produtos[numero_produto]['preco'])
+
+class Carrinho(Produtos):
     def __init__(self):
         pass
     
-    def adicionar_produto(self,produto,preco):
-        if len(produto) > 0:
-            self.lista_produtos.append(produto)
-        if preco > 0:
-            self.lista_precos.append(preco)
+    def adicionar_produto(self,numero_produto):
+        if numero_produto > 0:
+            self.lista_produtos.append(numero_produto)
 
     def mostra_total(self):
         total = 0
-        print(self.lista_produtos)
         if len(self.lista_precos) > 0:
             for preco in self.lista_precos:
                 total += preco
@@ -30,11 +25,13 @@ class Carrinho:
     def finalizar_compra(self):
         print("Compra finalizada")
 
-produtos = Produtos(["Lápis","Escova","Microfone"],[2.50,10,45])
-print(produtos.produtos_disponiveis())
+produtos = Produtos({
+    1:{'nome':'Lápis','preco':2.5},
+    2:{'nome':'Escova','preco':10},
+    3:{'nome':'Microfone','preco':45}
+})
 carrinho = Carrinho()
-carrinho.adicionar_produto("Lápis",2.50)
-carrinho.adicionar_produto("Escova",10)
+carrinho.adicionar_produto(1)
 print(f"Total no carrinho: {carrinho.mostra_total()}")
 print(carrinho.finalizar_compra())
 
@@ -44,7 +41,10 @@ def main():
         try:
             escolha = int(input("Digite uma opção: "))
             if escolha == 1:
-                ...
+                produtos.produtos_disponiveis()
+            elif escolha == 2:
+                numero_produto = int(input("Digite o número do produto? "))
+                carrinho.adicionar_produto(numero_produto)
         except ValueError:
             print("Você digitou algo errado")
 
