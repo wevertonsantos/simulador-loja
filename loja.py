fb9a79a5d9de1f7758cca1dd5449903123fd568b
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-07-11 16:32:21 -0300

    criando método adicionar produto e mostrando total.

--- loja.py
+++ loja.py
@@ -12,19 +12,24 @@ class Produtos:
             print(f"Produto: {self.nomes[i]}, Preço: {self.precos[i]}")
 
 class Carrinho:
-    def __init__(self,produtos,precos):
-        self.produtos = produtos
-        self.precos = precos
+    lista_produtos = {}
+    def __init__(self):
+        pass
+    
+    def adicionar_produto(self,produto,preco):
+        self.lista_produtos['produtos'] = produto
+        self.lista_produtos['precos'] = preco
 
     def mostra_total(self):
         total = 0
-        if len(self.produtos) > 0 and len(self.precos) > 0:
-            for i in range(len(self.produtos)):
-                total += self.precos[i]
+        if len(self.lista_produtos['produtos']) > 0 and len(self.lista_produtos['precos']) > 0:
+            for preco in self.lista_produtos['precos']:
+                total += preco
         return total
 
 pessoa1 = Pessoa("Rodrigo")
 produtos = Produtos(["Lápis","Escova","Microfone"],[2.50,10,45])
 print(produtos.produtos_disponiveis())
-carrinho = Carrinho(["Lápis","Escova"],[2.50,10])
+carrinho = Carrinho()
+carrinho.adicionar_produto(["Lápis"],[2.50])
 print(carrinho.mostra_total())
